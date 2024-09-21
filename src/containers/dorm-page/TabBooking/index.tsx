58e8e101d51f4e792e5feb750488d60402fd22a2
WoraponK <worapon.klabsr@gmail.com>
2024-09-21 11:49:41 +0700

    bugfix ui dorm booking

--- src/containers/dorm-page/TabBooking/index.tsx
+++ src/containers/dorm-page/TabBooking/index.tsx
@@ -35,16 +35,18 @@ const TabBooking: React.FC<Props> = ({ dormId, rooms, isCreator, isSuperuser, is
 
   return (
     <div className="space-y-8">
-      <div className="flex justify-center items-center">
-        <div className="border-2 border-primary py-2 px-4 w-fit rounded-full flex items-center justify-center space-x-2 text-primary">
-          <IoIosWarning />
-          <p className="font-semibold">
-            {!user && 'กรุณาเข้าสู่ระบบเพื่อทำการจองห้องพัก'}
-            {isSuperuser && 'เนื่องจากคุณมีบทบาทเป็นเจ้าของหอพัก จึงไม่สามารถทำการจองห้องพักได้'}
-            {isAdmin && 'เนื่องจากคุณเป็นผู้ดูแลระบบ จึงไม่สามารถทำการจองห้องพักได้'}
-          </p>
+      {!user || isSuperuser || isAdmin ? (
+        <div className="flex justify-center items-center">
+          <div className="border-2 border-primary py-2 px-4 w-fit rounded-full flex items-center justify-center space-x-2 text-primary">
+            <IoIosWarning />
+            <p className="font-semibold">
+              {!user && 'กรุณาเข้าสู่ระบบเพื่อทำการจองห้องพัก'}
+              {isSuperuser && 'เนื่องจากคุณมีบทบาทเป็นเจ้าของหอพัก จึงไม่สามารถทำการจองห้องพักได้'}
+              {isAdmin && 'เนื่องจากคุณเป็นผู้ดูแลระบบ จึงไม่สามารถทำการจองห้องพักได้'}
+            </p>
+          </div>
         </div>
-      </div>
+      ) : null}
       {rooms.length > 0 ? (
         <div className="grid grid-cols-4 gap-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-[480px]:grid-cols-1">
           {rooms.map((ele) => (
